fc4d8db12735aa586ecafa217e14582dfc9f2f55
tugberkugurlu <tugberkugurlu@gmail.com>
2016-06-24 15:47:23 +0200

    Add full name (bindings not working yet)

--- EvolvingSample/helloworld.jsx
+++ EvolvingSample/helloworld.jsx
@@ -2,7 +2,7 @@ var FirstName = React.createClass({
    render: function() {
        return (
            <div>
-                <strong>First name: </strong> <input type="text" placeholder="First name" />
+                <strong>First name: </strong> <input type="text" placeholder="First name" value={this.props.value} />
            </div>  
        );
    }
@@ -12,7 +12,17 @@ var LastName = React.createClass({
    render: function() {
        return (
            <div>
-                <strong>Last name: </strong> <input type="text" placeholder="Last name" />
+                <strong>Last name: </strong> <input type="text" placeholder="Last name" value={this.props.value} />
+           </div>  
+       );
+   }
+});
+
+var FullName = React.createClass({
+   render: function() {
+       return (
+           <div>
+                <span>{this.props.person.firstName}</span> <span>{this.props.person.lastName}</span>
            </div>  
        );
    }
@@ -22,14 +32,17 @@ var PersonName = React.createClass({
     render: function() {
         return (
             <div>
-                <FirstName />
-                <LastName />
+                <FirstName value={this.props.person.firstName} />
+                <LastName value={this.props.person.lastName} />
+                <FullName person={this.props.person} />
             </div>          
         );
     } 
 });
 
+var person = { firstName: '', lastName: '' };
+
 ReactDOM.render(
-    <PersonName />,
+    <PersonName person={person} />,
     document.getElementById('container')
 );
