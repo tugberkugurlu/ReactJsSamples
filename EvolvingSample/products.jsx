07bf2a91b09d74a5cbf459793b6b35ee8df6355c
tugberkugurlu <tugberkugurlu@gmail.com>
2016-06-24 15:59:46 +0100

    itirate over products list

---
+++ EvolvingSample/products.jsx
@@ -0,0 +1,38 @@
+var ProductsTable = React.createClass({
+    render: function() {
+        var rows = [];
+        
+        this.props.products.forEach(function(product) {
+            rows.push(<tr key={product.name}> 
+                <td>{product.name}</td> 
+                <td>{product.price}</td> 
+            </tr>) 
+        });
+        
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+});
+
+var PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+ReactDOM.render(
+    <ProductsTable products={PRODUCTS} />,
+    document.getElementById('products')
+)
