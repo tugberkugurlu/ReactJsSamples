60b6bf2f359335126650cd0c0c089fdc9a621668
Tugberk Ugurlu <tugberk@outlook.com>
2016-07-01 17:25:28 +0100

    cache busting
     - output file now caries the git hash
     - webpack generates the html now with the output files put there based on the template
     - webpack now generates webpack-assets.json file which can be used by the server side to read the assets file.
    
    Good article on the topic: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.k8x2lm4xb

--- WebpackSample/webpack.config.js
+++ WebpackSample/webpack.config.js
@@ -1,6 +1,19 @@
 var webpack = require('webpack');
+var GitSHAPlugin = require('git-sha-webpack-plugin');
+var AssetsPlugin = require('assets-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var definedPlugins = process.env.NODE_ENV !== 'development' ? [
+var basePlugins = [
+    new GitSHAPlugin({shaLength: 20}), 
+    new AssetsPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'Hello Webpack',
+        template: 'index.ejs', // Load a custom template (ejs by default but can be changed)
+        inject: 'body' // Inject all scripts into the body (this is the default so you can skip it)
+    })
+];
+
+var envBasedPlugins = process.env.NODE_ENV !== 'development' ? [
     new webpack.optimize.UglifyJsPlugin({
         compress: {
             warnings: false
@@ -15,14 +28,13 @@ module.exports = {
     entry: './entry.js',
     output: {
         path: __dirname,
-        filename: 'bundle.js'
+        filename: 'bundle.[chunkgitsha].js'
     },
     module: {
         loaders: [{
             test: /\.scss$/,
-            loaders: ["style", "css", "sass"]
+            loaders: ['style', 'css', 'sass']
         }]
     },
-
-    plugins: definedPlugins
+    plugins: basePlugins.concat(envBasedPlugins)
 };
