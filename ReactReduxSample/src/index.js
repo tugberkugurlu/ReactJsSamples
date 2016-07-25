2d23ba5fc210b920baf54723a36a29fe6b56d8b7
tugberkugurlu <tugberk@outlook.com>
2016-07-26 00:21:09 +0100

    messaging component with internal state

--- ReactReduxSample/src/index.js
+++ ReactReduxSample/src/index.js
@@ -1,4 +1,5 @@
 require('./bootswatch.less');
+require('./messaging.scss');
 var React = require('react');
 var ReactDom = require('react-dom');
 import { applyMiddleware, createStore } from 'redux';
@@ -42,14 +43,72 @@ var HelloWorld = React.createClass({
     }
 });
 
+var Messaging = React.createClass({
+    onMessageSent: function () {
+        const newMessages = this.state.messages.concat([{
+            from: this.props.userName,
+            content: this.state.messageToSend
+        }]);
+
+        this.setState({
+            messages: newMessages,
+            messageToSend: ''
+        });
+    },
+    getInitialState: function () {
+        return {
+            messageToSend: '',
+            unreadCount: 0,
+            messages: []
+        };
+    },
+    render: function () {
+        return <div id="messaging">
+            <div className="header row">
+                <div className="logo col-xs-8">M</div>
+                <div className="username col-xs-2">({this.props.userName})</div>
+                <div className="unread-count col-xs-2">{this.state.unreadCount}</div>
+            </div>
+
+            <div className="messages row">
+                <div className="col-xs-12">
+                    {this.state.messages.map(msg => 
+                        <div className="message row">
+                            <div className="from col-xs-3" title={msg.sentAt}>{msg.from}:</div>
+                            <div className="content col-xs-">{msg.content}</div>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            <div className="control-panel row no-gutter">
+                <div className="col-xs-9">
+                    <input className="form-control" type="text" 
+                        placeholder="write your message..." 
+                        value={this.state.messageToSend} 
+                        onChange={(e) => this.setState({ messageToSend: e.currentTarget.value })} />
+                </div>
+                <div className="col-xs-3">
+                    <button onClick={this.onMessageSent} type="button" className="btn btn-primary">Send</button>
+                </div>
+            </div>
+        </div>;
+    }
+});
+
 document.addEventListener('click', () => {
     store.dispatch({ type: 'INCREMENT' });
 });
 
 ReactDom.render(
     <div className="row">
-        <div className="col-sm-6"><HelloWorld /></div>
-        <div className="col-sm-6"><HelloWorld /></div>
+        <div className="col-sm-12">
+            <div className="row">
+                <div className="col-sm-2"><HelloWorld /></div>
+                <div className="col-sm-5"><Messaging userName="Bob" /></div>
+                <div className="col-sm-5"><Messaging userName="Alice" /></div>
+            </div>
+        </div>
     </div>, 
     document.getElementById('app')
 );
