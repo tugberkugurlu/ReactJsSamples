cad4755f645e6a109e37a7c166b27cf8ce173c3f
tugberkugurlu <tugberk@outlook.com>
2016-07-27 21:58:51 +0100

    store, reducer

--- ReduxMultiactionInitSample/src/index.js
+++ ReduxMultiactionInitSample/src/index.js
@@ -1,8 +1,41 @@
 import './index.scss'
 import 'loaders.css/loaders.css'
-import React from 'react';
+import React, { PropTypes } from 'react';
 import ReactDom from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { connect } from 'react-redux';
 import client from './client';
+
+const initialState = {
+    appStatus: 'initializing',
+    currentUser: null,
+    categories: []
+};
+
+const APP_INITIALIZATION_STARTED = 'APP_INITIALIZATION_STARTED';
+const APP_INITIALIZED = 'APP_INITIALIZED';
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case APP_INITIALIZATION_STARTED:
+            return Object.assign({}, state, { appStatus: 'initializing'});
+    
+        case APP_INITIALIZED:
+            return Object.assign({}, state, { 
+                appStatus: 'initialized', 
+                currentUser: action.user,
+                categories: state.categories.concat(action.categories) 
+            });
+
+        default:
+            return state;
+    }
+};
+
+const logger = createLogger(),
+    store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 var LoadingIndicator = () => {
     return <div className="loading-indicator-holder">
@@ -10,18 +43,25 @@ var LoadingIndicator = () => {
     </div>;
 }
 
-var HelloWorld = () => {
+var App = React.createClass({
+    getInitialState: () => {
+        return {
+            isInitializing: true
+        };
+    },
 
-    if(false) {
-        return <LoadingIndicator />;
+    render: function () {
+        if(this.state.isInitializing) {
+            return <LoadingIndicator />;
+        }
+
+        return <div>
+            Hello World!
+        </div>;
     }
-
-    return <div>
-        Hello World!
-    </div>;
-};
+});
 
 ReactDom.render(
-    <HelloWorld />, 
+    <App />, 
     document.getElementById('app')
 );
