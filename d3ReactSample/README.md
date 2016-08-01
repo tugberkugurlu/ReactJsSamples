0024a0d99feb4b8ee02ca2c7a6cbd6fdd8885380
tugberkugurlu <tugberk@outlook.com>
2016-08-01 20:45:25 +0100

    drawing rect on screen, this is my first d3 shape :tada:

--- d3ReactSample/README.md
+++ d3ReactSample/README.md
@@ -2,3 +2,7 @@
 
  - [using d3.js as an external in webpack](http://stackoverflow.com/questions/31616235/using-d3-js-as-an-external-in-webpack)
  - [Use Webpack with D3 Dashboard Pages](http://fengshuo.co/2015/06/27/use-webpack-with-d3-dashboard-pages/)
+ - [D3 version 4.0](https://github.com/visfest/topics2015/blob/b7fcab9f481b12948fb96df3bafb9a5997c57816/d3-v4.md)
+ - [Donut Chart Sample](http://jsfiddle.net/gregfedorov/qh9x5/9/)
+ - [D3 within React the right way](http://oli.me.uk/2015/09/09/d3-within-react-the-right-way/)
+ - [React.js By Example: Interacting with the DOM](http://jamesknelson.com/react-js-by-example-interacting-with-the-dom/)
--- d3ReactSample/src/index.js
+++ d3ReactSample/src/index.js
@@ -2,11 +2,21 @@ var React = require('react');
 var ReactDom = require('react-dom');
 var d3 = require('d3');
 
-console.log(d3.version);
-
-var HelloWorld = () => {
-    return <div>Hello World!</div>;
-};
+var HelloWorld = React.createClass({
+    componentDidMount: function () {
+        var el = ReactDom.findDOMNode(this);
+        var svg = d3.select(el)
+            .append('svg')
+                .attr('class', 'd3')
+            .append('rect')
+                .attr('width', 50)
+                .attr('height', 200)
+                .style('fill', 'blue');
+    },
+    render: function () {
+        return <div></div>;
+    }
+});
 
 ReactDom.render(
     <HelloWorld />, 
