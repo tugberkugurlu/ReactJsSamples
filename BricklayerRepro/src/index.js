f8cf2f2d958e597cb9aa3129c7d5c2f045f7f3f9
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2016-10-13 20:49:44 +0100

    repro bricklayer update issue

--- BricklayerRepro/src/index.js
+++ BricklayerRepro/src/index.js
@@ -1,9 +1,20 @@
 import './bootswatch.less';
 import 'bricklayer/dist/bricklayer.css';
+import './index.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
+import Bricklayer from 'bricklayer';
+import loremIpsum from 'lorem-ipsum';
+import _ from 'underscore';
 
 const ItemsList = React.createClass({
+    componentDidMount: function() {
+        this.itemsListContainerBricklayer = new Bricklayer(this.refs.itemsListContainer);
+    },
+    componentDidUpdate: function() {
+        this.itemsListContainerBricklayer.destroy();
+        this.itemsListContainerBricklayer = new Bricklayer(this.refs.itemsListContainer);
+    },
     render: function() {
         const { items } = this.props;
         return <div className="search-results-container">
@@ -11,7 +22,7 @@ const ItemsList = React.createClass({
                 <div className="col-xs-12">
                     <div className="bricklayer cards-list" ref="itemsListContainer">
                         {items.map(item =>
-                            <div className="card">{item.name}</div> 
+                            <div key={item.id} className="card">{item.content}</div> 
                         )}
                     </div>
                 </div>
@@ -20,9 +31,47 @@ const ItemsList = React.createClass({
     }
 });
 
-const HelloWorld = () => {
-    return <div>hey!</div>;
+export const guid = () => {
+    const s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    };
+
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
 };
+
+const getRandomItems = () => {
+    const count = _.random(20, 100);
+    return _(_.range(count)).map(i => ({
+        id: guid(),
+        content: loremIpsum()
+    }));
+};
+
+const HelloWorld = React.createClass({
+    getInitialState: function() {
+        return {
+            items: getRandomItems()
+        };
+    },
+    componentDidMount: function() {
+        this.timerId = window.setInterval(() => {
+            this.setState({
+                items: getRandomItems()
+            });
+        }, 3000);
+    },
+    componentWillUnmount: function() {
+        window.clearInterval(this.timerId);
+    },
+    render: function() {
+        return <div>
+            <ItemsList items={this.state.items} />
+        </div>;
+    }
+});
 
 ReactDom.render(
     <HelloWorld />, 
