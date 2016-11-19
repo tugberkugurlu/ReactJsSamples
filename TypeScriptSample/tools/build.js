9bc6d4dbdef3f7eac8d7621fe834142bfae1933d
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2016-11-19 20:02:10 +0000

    force webpack to fail the build

--- TypeScriptSample/tools/build.js
+++ TypeScriptSample/tools/build.js
@@ -1,6 +1,6 @@
-var webpack = require('webpack');
-var webpackConfig = require('../webpack.config.prod');
-var colors = require('colors');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.prod');
+const colors = require('colors');
 
 process.env.NODE_ENV = 'production'; // this assures the Babel dev config (for hot reloading) doesn't apply.
 
@@ -9,27 +9,16 @@ console.log('Generating minified bundle for production via Webpack. This will ta
 webpack(webpackConfig).run(function (err, stats) {
     if (err) { // so a fatal error occurred. Stop here.
         console.log(err.bold.red);
-        return 1;
+        process.exit(-1);
     }
 
-    var jsonStats = stats.toJson();
+    const jsonStats = stats.toJson();
+    console.log('Webpack stats: ' + stats);
 
-    if (jsonStats.hasErrors) {
-        return jsonStats.errors.map(function (error) {
-            console.log(error.red);
-        });
+    if (jsonStats.errors.length > 0) {
+        process.exit(jsonStats.errors.length);
+    } else {
+        console.log('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!'.green);
+        return 0;
     }
-
-    if (jsonStats.hasWarnings) {
-        console.log('Webpack generated the following warnings: '.bold.yellow);
-        jsonStats.warnings.map(function (warning) {
-            console.log(warning.yellow);
-        });
-    }
-
-    console.log(`Webpack stats: ${stats}`);
-
-    console.log('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!'.green);
-
-    return 0;
 });
